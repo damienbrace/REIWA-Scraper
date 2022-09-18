0553702ea015b7a4c2315d0b370ad1cdf45d33c4
Damien Brace <damienbrace@gmail.com>
2022-09-18 18:49:09 +0800

    converted house numbers to int

--- script.py
+++ script.py
@@ -8,4 +8,7 @@ html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 
 noOfHouses = soup.find("div", {"class": "homeSearch-propertyCounts"})
-print(noOfHouses.text.split()[0])
+noOfHouses = noOfHouses.text.split()[0].split(',')
+noOfHouses = int(noOfHouses[0] + noOfHouses[1])
+print(noOfHouses)
+
